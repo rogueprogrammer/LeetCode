cd575b4bf8f346a0031e1eb54bde2886f18dde57
rogueprogrammer <dkhanna@uwaterloo.ca>
2015-03-20 18:53:03 -0400

    Create Header.h

---
+++ Sorting-Searching/Header.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unordered_map>
+#include <Queue> //priority queue
+#include <math.h>
+using namespace std;
+
+#endif
