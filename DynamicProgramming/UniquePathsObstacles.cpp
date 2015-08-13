6d580f704d1e192fd50a17ad8fd757847777329a
Dev Khanna <dkhanna@uwaterloo.ca>
2015-08-13 17:51:33 -0400

    Update UniquePathsObstacles.cpp

--- DynamicProgramming/UniquePathsObstacles.cpp
+++ DynamicProgramming/UniquePathsObstacles.cpp
@@ -4,12 +4,9 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
-//https://leetcode.com/problems/unique-paths-ii/
-
-
 #define DEBUG_VEC(x) for(int i = 0; i < x.size(); ++i){ for(int j = 0; j < x[0].size(); ++ j){cout << x[i][j] << " ";} cout << endl;}
 
+//https://leetcode.com/problems/unique-paths-ii/
 
 int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
 	int num_rows = obstacleGrid.size();
@@ -26,13 +23,10 @@ int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
 
 	for (int i = 1; i < num_rows; ++i){
 		for (int j = 1; j < num_cols; ++j){
-			if (obstacleGrid[i][j] == 1) { dp[i][j] = 0; }
-			else{
-				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-			}
+			dp[i][j] = (obstacleGrid[i][j] == 1) ? 0 : dp[i - 1][j] + dp[i][j - 1];
 		}
 	}
-	//DEBUG_VEC(dp);
+	DEBUG_VEC(dp);
 
 	return dp[num_rows - 1][num_cols - 1];
 }
