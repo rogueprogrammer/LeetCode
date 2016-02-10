d8beb4936c839fdaf8b00da1068520fe1cd8a66e
rogueprogrammer <dkhanna@uwaterloo.ca>
2016-02-10 14:20:41 -0500

    Update CoinChange.cpp

--- DynamicProgramming/CoinChange.cpp
+++ DynamicProgramming/CoinChange.cpp
@@ -1,20 +1,48 @@
-#include "header.h"
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <map>
+using namespace std;
+#define DEBUG_1D(x) for(int i = 0; i < x.size(); ++i){cout << x[i] << " ";} cout << endl;
+#define DEBUG_2D(x) for(int i = 0; i < x.size(); ++i){ for(int j = 0; j < x[0].size(); ++ j){cout << x[i][j] << " ";} cout << endl;}
 
-//Given an amount, and a vector v containing coin denomination values, finds the minimum # of coins to make the amount.
-int CoinChange(int amount, vector<int> v){
-
-	vector<int> C(amount);
-	C[0] = 0;
-	for (int i = 1; i < amount; ++i){
-		
-		int num_coins = 0;
-		int min_num_coins = INT_MAX;
-		for (int j = 0; j < v.size(); ++j){
-			if (i < v[j]) break;
-			num_coins = C[i - v[j]] + 1;
-			if (num_coins < min_num_coins) min_num_coins = num_coins;
+int CoinChange(int amount, vector<int> C){
+	int numCoins = C.size();
+	vector<int> DP(amount + 1);
+	DP[0] = 0;
+	for (int i = 1; i <= amount; ++i){
+		int minCoins = INT_MAX;
+		for (int j = 0; j < C.size(); ++j){
+			if (C[j] > i) break;
+			if (i == C[j]) {
+				minCoins = 1;
+				break;
+			}
+			minCoins = min(minCoins, DP[i - C[j]] + DP[C[j]]);
 		}
-		C[i] = min_num_coins;
+		DP[i] = minCoins;
 	}
-	return C[amount - 1];
+	return DP[amount];
 }
+
+
+int main(){
+
+	int amount = 11;
+	vector<int> v = { 1, 2, 5, 10 };
+	cout << CoinChange(amount, v) << endl;
+
+	return 0;
+}
+
+
+
